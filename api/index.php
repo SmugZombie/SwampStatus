f29996f73b8372a3919354221baa803036098cf3
Ron Egli <ron@r-egli.com>
2018-07-12 21:38:10 -0700

    Added Toggle Function for Swamp_Button integration

--- api/index.php
+++ api/index.php
@@ -24,9 +24,20 @@ if($action == "set"){
 elseif($action == "get"){
 	echo file_get_contents($status_file);
 }
+elseif($action == "toggle"){
 
+        $current_status = json_decode(file_get_contents($status_file),true)['status'];
+        if($current_status == 0){ $new_status = 1; }else{ $new_status = 0; }
+        $newJSON = array();
+        $newJSON['status'] = $new_status;
+        $newJSON['duration'] = "";
+        $newJSON['updated'] = date('Y-m-d H:i');
+        $newJSON['purpose'] = "SWAMP BUTTON";
 
+        file_put_contents($status_file, json_encode($newJSON));
+        echo file_get_contents($status_file);
 
+}
 
 function cleanInput($input){
 	return trim(preg_replace('/ +/', ' ', preg_replace('/[^A-Za-z0-9 ]/', ' ', urldecode(html_entity_decode(strip_tags($input))))));
